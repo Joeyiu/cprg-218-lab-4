c7e2ca59c0441c96d0ecf4989c152a8f2ce1e87c
Joeyiu <147527603+Joeyiu@users.noreply.github.com>
2024-03-07 13:53:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
